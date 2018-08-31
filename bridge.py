76256bf82ef132158d9ba2dd81efddab449433bc
Alistair Buxton <a.j.buxton@gmail.com>
2018-08-31 14:23:15 +0100

    Add python SDL to Switch bridge
    This code is incomplete. It uses the SDL Game Controller subsystem
    which is easy to use because it makes all controllers look like
    an XBOX 360 controller. Unfortunately it doesn't have enough buttons
    for every switch button.
    
    See "./bridge.py --help" for usage. You can specify the controller
    by number or name, and set the port and baudrate. The default baud
    rate matches the one in Joystick.c as of this commit. The other
    defaults should work if you have only one controller.
    
    Rather than sending an update for every event, this sends an update
    every ~5 msec (subject to python timing inaccuracy). It seems very
    responsive compared to previous versions.
    
    In order to get the full joystick mapping working the code should
    be modified to use SDL_Joystick instead of SDL_GameController
    however this code will be joystick-specific as there is no standard
    mapping for SDL_Joystick.

---
+++ bridge.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env python3
+
+
+import argparse
+
+import sdl2
+import sdl2.ext
+import struct
+import binascii
+import serial
+import time
+
+def enumerate_controllers():
+    print('Controllers connected to this system:')
+    for n in range(sdl2.SDL_NumJoysticks()):
+        name = sdl2.SDL_JoystickNameForIndex(n)
+        if name is not None:
+            name = name.decode('utf8')
+        print(n, ':', name)
+    print('Note: These are numbered by connection order. Numbers will change if you unplug a controller.')
+
+
+def get_controller(c):
+    try:
+        n = int(c, 10)
+        return sdl2.SDL_GameControllerOpen(n)
+    except ValueError:
+        for n in range(sdl2.SDL_NumJoysticks()):
+            name = sdl2.SDL_JoystickNameForIndex(n)
+            if name is not None:
+                name = name.decode('utf8')
+                if name == c:
+                    return sdl2.SDL_GameControllerOpen(n)
+        raise Exception('Controller not found: %s'.format(c))
+
+
+buttonmapping = [
+    sdl2.SDL_CONTROLLER_BUTTON_Y, # Y
+    sdl2.SDL_CONTROLLER_BUTTON_B, # B
+    sdl2.SDL_CONTROLLER_BUTTON_A, # A
+    sdl2.SDL_CONTROLLER_BUTTON_X, # X
+    sdl2.SDL_CONTROLLER_BUTTON_LEFTSHOULDER, # L
+    sdl2.SDL_CONTROLLER_BUTTON_RIGHTSHOULDER, #R
+    sdl2.SDL_CONTROLLER_BUTTON_LEFTSTICK, # ZL
+    sdl2.SDL_CONTROLLER_BUTTON_RIGHTSTICK, # ZR
+    sdl2.SDL_CONTROLLER_BUTTON_BACK, # SELECT
+    sdl2.SDL_CONTROLLER_BUTTON_START, # START
+    sdl2.SDL_CONTROLLER_BUTTON_LEFTSTICK, # LCLICK
+    sdl2.SDL_CONTROLLER_BUTTON_RIGHTSTICK, # RCLICK
+    sdl2.SDL_CONTROLLER_BUTTON_GUIDE, # HOME
+    sdl2.SDL_CONTROLLER_BUTTON_GUIDE, # CAPTURE
+]
+
+
+
+def send_state(ser, controller):
+    buttons = sum([sdl2.SDL_GameControllerGetButton(controller, b)<<n for n,b in enumerate(buttonmapping)])
+    hat = 0
+    lx = (sdl2.SDL_GameControllerGetAxis(controller, 0) >> 8) + 128
+    ly = (sdl2.SDL_GameControllerGetAxis(controller, 1) >> 8) + 128
+    rx = (sdl2.SDL_GameControllerGetAxis(controller, 2) >> 8) + 128
+    ry = (sdl2.SDL_GameControllerGetAxis(controller, 3) >> 8) + 128
+
+    bytes = struct.pack('>BHBBBB', hat, buttons, lx, ly, rx, ry)
+    message = binascii.hexlify(bytes)
+    ser.write(message + b'\n')
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-l', '--list-controllers', action='store_true', help='Display a list of controllers attached to the system.')
+    parser.add_argument('-c', '--controller', type=str, default='0', help='Controller to use. Default: 0.')
+    parser.add_argument('-b', '--baud-rate', type=int, default=115200, help='Baud rate. Default: 57600.')
+    parser.add_argument('-p', '--port', type=str, default='/dev/ttyUSB0', help='Serial port. Default: /dev/ttyUSB0.')
+
+    args = parser.parse_args()
+
+    sdl2.SDL_Init(sdl2.SDL_INIT_GAMECONTROLLER)
+
+    if args.list_controllers:
+        enumerate_controllers()
+        exit(0)
+
+
+    controller = get_controller(args.controller)
+    try:
+        print('Using "{:s}" for input.'.format(sdl2.SDL_JoystickName(sdl2.SDL_GameControllerGetJoystick(controller)).decode('utf8')))
+    except AttributeError:
+        print('Using controller {:s} for input.'.format(args.controller))
+
+    ser = serial.Serial(args.port, args.baud_rate)
+    print('Using {:s} at {:d} baud for comms.'.format(args.port, args.baud_rate))
+
+    while True:
+        sent = False
+        for event in sdl2.ext.get_events():
+            pass
+
+        if not sent:
+            send_state(ser, controller)
+
+        time.sleep(0.005)
